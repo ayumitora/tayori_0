9d2f64b9d7d78cc81d0ab3f21500ea02a19a1ee3
ayumi_arai <0araiayumi@gmail.com>
2019-09-05 13:22:50 +0900

    メーカー登録アクション追加

--- app/controllers/makers_controller.rb
+++ app/controllers/makers_controller.rb
@@ -9,6 +9,18 @@ class MakersController < ApplicationController
     @maker = Maker.new
   end
 
+  def create
+    maker = Maker.new(maker_params)
+    maker.save!
+    redirect_to makers_url, notice: "メーカー「#{maker.name}を登録しました。」"
+  end
+
   def edit
   end
+end
+
+private
+
+def maker_params
+  params.require(:maker).permit(:name, :mail, :password)
 end
