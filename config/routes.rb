3adc42f4dac9db541af6323818c4ed6276b0b1f9
ayumi_arai <0araiayumi@gmail.com>
2019-09-06 15:43:07 +0900

    productsコントローラ、ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
+  get 'products/new'
+  get 'products/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
