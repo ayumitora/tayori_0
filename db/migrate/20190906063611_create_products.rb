c8f8c68a6c6e381dd19dac8ce61037b9c9f30f45
ayumi_arai <0araiayumi@gmail.com>
2019-09-06 15:40:10 +0900

    productモデル作成

---
+++ db/migrate/20190906063611_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.references :maker, foreign_key: true
+      t.string :name, null: false
+      t.integer :price
+      t.string :image
+      t.string :season
+
+      t.timestamps
+    end
+  end
+end
