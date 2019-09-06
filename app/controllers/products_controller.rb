2a3a25670301dfa6b4c41cd467f6c4a91be0dc4c
ayumi_arai <0araiayumi@gmail.com>
2019-09-06 15:55:36 +0900

    productコントローラ大枠作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,28 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
+    @products = Product.find(params[:id])
   end
 
   def new
+    @products = Product.new
+  end
+
+  def create
+    product = Product.new(product_params)
+    puroduct.save!
+    redirect_to productsurl, notice: "プロダクト「#{product.name}を登録しました。」"
   end
 
   def edit
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :image, :season)
   end
 end
