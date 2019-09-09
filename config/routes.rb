7ba432b54e51dbdfd139d3c3cadf753a704cce2c
ayumi_arai <0araiayumi@gmail.com>
2019-09-09 14:49:02 +0900

    gem deviceにてmakerモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :makers
   resources :products
-  root to: 'products#index'
+  root to: 'makers#index'
   post '/products/new' => 'products#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
