1e2dc0674f37703e5513e9d52219b2380e7c53d7
ayumi_arai <0araiayumi@gmail.com>
2019-09-09 12:03:51 +0900

    プロダクト詳細大枠

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   end
 
   def show
-    # @products = Product.find(params[:id])
+    @product = Product.find(params[:id])
   end
 
   def new
@@ -18,6 +18,7 @@ class ProductsController < ApplicationController
   end
 
   def edit
+
   end
 
   private
