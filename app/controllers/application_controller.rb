d44781cfc04e411d7fdddfa86cc456b708e62ede
ayumi_arai <0araiayumi@gmail.com>
2019-09-10 14:41:51 +0900

    ログアウト時のRouting Error解決

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  private
+
+  # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource_or_scope)
+      products_path
+  end
+
+  # ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource_or_scope)
+      products_path
+  end
 end
