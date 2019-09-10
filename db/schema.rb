a5647d1d002ba38ad14a4a118498dcc12f502274
ayumi_arai <0araiayumi@gmail.com>
2019-09-10 15:08:19 +0900

    Makerのカラム,view追加、rails g devise:views makers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_064314) do
+ActiveRecord::Schema.define(version: 2019_09_10_055427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 2019_09_09_064314) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", default: "", null: false
+    t.string "site_url"
+    t.text "overview"
+    t.string "logo"
+    t.string "image"
+    t.string "ivent"
     t.index ["email"], name: "index_makers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_makers_on_reset_password_token", unique: true
   end
