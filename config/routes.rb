413d73b4cbeda973c3d6251d2640dd807b2c6793
ayumi_arai <0araiayumi@gmail.com>
2019-09-10 17:24:41 +0900

    rails generate devise：controllers makers実行、初期設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :makers
+  devise_for :makers, controllers: {
+    sessions: 'makers/sessions'
+  }
+
   resources :products
   root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
