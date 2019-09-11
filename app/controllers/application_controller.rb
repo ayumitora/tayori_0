e87b9a20a30bcb4721eb0579e9f3e92ed1ea3741
ayumi_arai <0araiayumi@gmail.com>
2019-09-11 11:52:40 +0900

    makers_controller作成、編集、詳細などのviewの大枠

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,6 @@ class ApplicationController < ActionController::Base
 
   # ログアウト後のリダイレクト先
   def after_sign_out_path_for(resource_or_scope)
-      products_path
+      makers_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'makers/index'
+  get 'makers/show'
+  get 'makers/new'
+  get 'makers/edit'
   devise_for :makers, controllers: {
     sessions: 'makers/sessions'
   }
 
   resources :products
+  resources :makers
   root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
