d7f36d0cad8caa834a00fe002530e947ad08a541
ayumi_arai <0araiayumi@gmail.com>
2019-09-12 14:57:39 +0900

    バリデーション追加

--- app/models/maker.rb
+++ app/models/maker.rb
@@ -4,6 +4,16 @@ class Maker < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+  validates :mail, presence: true
+  validates :site_url,
+    presence: true,
+    on: :update ##updateメソッドのみ有効
+  validates :overview,
+    presence: true,
+    on: :update ##updateメソッドのみ有効
+
+
   has_many :products
 
     mount_uploader :image, MakerImageUploader
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
   belongs_to :maker
   mount_uploader :image, ProductImageUploader
 
