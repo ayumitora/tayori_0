a0f43b68eed96ba14b7b449a12bdb5c5341e70a4
ayumi_arai <0araiayumi@gmail.com>
2019-09-19 14:28:09 +0900

    rails g devise:controllers customers

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,28 @@ Rails.application.routes.draw do
   get 'home/show'
   root to: "home#index"
 
-  devise_for :customers
+  devise_for :customers, controllers: {
+        sessions: 'customers/sessions'
+  }
+
   devise_for :makers, controllers: {
     sessions: 'makers/sessions'
   }
+
   resources :evaluates
   resources :products
   resources :makers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+# 例
+#   devise_for :users, :controllers => {
+#     :registrations => 'users/registrations',
+#     :sessions => 'users/sessions'
+#   }
+
+#   devise_scope :user do
+#     get "sign_in", :to => "users/sessions#new"
+#     get "sign_out", :to => "users/sessions#destroy"
+#   end
