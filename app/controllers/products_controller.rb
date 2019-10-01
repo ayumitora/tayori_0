018b354ca252d4b9f98a7a66a749df9d7901d78a
ayumi_arai <0araiayumi@gmail.com>
2019-10-01 13:06:30 +0900

    質問解決、hidden_fieldでformに情報をもたせる

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,6 @@ class ProductsController < ApplicationController
   before_action :authenticate_maker!, except: [:index, :show]
 
   def index
-      # @products = current_maker.products
-    # @products = Product.all
-    # customer_signed_in?
     @products = Product.where(maker_id: params[:maker_id])
     @maker = Maker.find_by(id: params[:maker_id])
     # 自分が評価したプロダクトを除外する？？
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,7 @@
 module ProductsHelper
+  # def evaluate_fixed
+  #   evaluates = Evaluate.where(customer_id: current_customer.id)
+  #   # ログインしているカスタマーによる評価テーブルを取り出す
+  #   product.id == each evaluates do evaluates.where(product_id: )
+  # end
 end
