aeef3452e1b189782718b8d9a3e650986b7e7a4b
ayumi_arai <0araiayumi@gmail.com>
2019-10-08 12:30:47 +0900

    評価をしてくれたカスタマーにスコア付与のコールバック、エラーを質問するためにコミット

--- app/models/evaluate.rb
+++ app/models/evaluate.rb
@@ -1,7 +1,16 @@
 class Evaluate < ApplicationRecord
+  after_save :grant_score
   belongs_to :customer
   belongs_to :product
   has_many :maker_comments, dependent: :destroy
   has_many :customer_comments, dependent: :destroy
   mount_uploader :image, EvaluateImageUploader
+
+  private
+
+  def grant_score
+    customer = self.customer
+    customer.score += 10
+    customer.save!
+  end
 end
