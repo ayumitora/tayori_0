b82eba422fd4b84fb654d5287efa5ebc3c315432
ayumi_arai <0araiayumi@gmail.com>
2019-10-11 16:17:33 +0900

    herokuにpushしてみるためのコミット

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  #herokuエラーにより追記
+  config.assets.initialize_on_precompile = false
+
 end
